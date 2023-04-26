9e657a4483352c43ad46ca07694f7a266af29d84
abyssnlp <rawatshaurya1994@gmail.com>
2023-04-26 18:55:57 +0200

    Add feature/buffer

--- airflow_metrics_gbq/metrics.py
+++ airflow_metrics_gbq/metrics.py
@@ -1,15 +1,28 @@
+import concurrent.futures
+import multiprocessing
 import os
 import socket
 import time
-import random
-from typing import Optional
+import threading
+import queue
+from queue import Queue
+from concurrent.futures import ThreadPoolExecutor
+import atexit
+import typing as t
 from enum import Enum, unique
 from collections import defaultdict
 from dataclasses import dataclass
-from multiprocessing import Queue
 import pandas as pd
+from tenacity import (
+    retry,
+    retry_if_exception_type,
+    wait_fixed,
+    wait_random,
+    stop_after_attempt,
+)
 
 from airflow_metrics_gbq.utils import GoogleBigQueryConnector, setup_gcloud_logging
+from airflow_metrics_gbq.exceptions import NoMetricFoundException
 
 
 # pylint: disable=too-few-public-methods
@@ -21,8 +34,8 @@ class Point:
     domain: str
     value: float
     timestamp: float
-    check: Optional[str] = None
-    name: Optional[str] = None
+    check: t.Optional[str] = None
+    name: t.Optional[str] = None
 
 
 @unique
@@ -83,12 +96,12 @@ class PointWithType:
         return PointWithType(point, mtype)
 
 
-# TODO: Run in separate process
 # pylint: disable=too-many-instance-attributes
 class AirflowMonitor:
     """Main class to ship metrics to GBQ"""
 
-    CAPACITY = 500
+    CAPACITY: t.Final = 500
+    BATCH_TIME: t.Final = 5
 
     def __init__(
         self,
@@ -99,6 +112,7 @@ class AirflowMonitor:
         counts_table: str,
         last_table: str,
         timers_table: str,
+        num_threads: int = (multiprocessing.cpu_count() // 2),
     ):
         self.dataset_id = dataset_id
         self.counts_table = counts_table
@@ -107,27 +121,101 @@ class AirflowMonitor:
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = gcp_credentials
         self.gbq_connector = GoogleBigQueryConnector(gcp_credentials)
         self.logger = setup_gcloud_logging("airflow_monitoring", gcp_credentials)
+        # track batch time
+        self._last_flush = time.time()
         # buffer
-        self._buffer = Queue()
-        self._current = 0
+        self.pool = ThreadPoolExecutor(max_workers=num_threads)
+        self._metrics = []
+        self._buffer = Queue(maxsize=self.CAPACITY + 50)
+        self.monitor_batch = threading.Event()
+        self.monitor_batch.set()
+        self.pool.submit(self.monitor).add_done_callback(self.callback)
+
+        # Flush running
+        self.is_flush_running = threading.Event()
+        self.is_flush_running.clear()
+
+        # Fetch into queue from socket
+        self.pool.submit(self._fetch).add_done_callback(self.callback)
+
+        # Metrics connection
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._sock.bind((host, port))
 
-    def _fetch(self):
-        self._current = 0
-        self._buffer = []
-        while self._current <= self.CAPACITY:
-            # TODO: Add error handling if empty buffer or df, time based buffer
-            measure: str = self._sock.recv(1024).decode("utf-8")
-            try:
-                self._buffer.append(PointWithType.from_record(measure))
-                self._current += 1
-            except IndexError as e:
-                self.logger.error(f"Seems like there is no record, measure: {measure}, error: {e}")
+        atexit.register(self.close)
 
-    def _get_dfs(self) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
-        self._fetch()
-        measure_dict = self._part_types()
+    def monitor(self):
+        """Monitor batch time for flushing metrics"""
+        while self.monitor_batch.is_set():
+            if not self.is_flush_running.is_set():
+                self.is_flush_running.set()
+                if time.time() - self._last_flush > self.BATCH_TIME:
+                    if not self._buffer.empty():
+                        self.logger.info("Flushing queue, batch time exceeded")
+                        self.flush_queue()
+                self.is_flush_running.clear()
+            time.sleep(self.BATCH_TIME)
+
+    @staticmethod
+    def callback(future: concurrent.futures.Future):
+        """Future callback"""
+        exc = future.exception()
+        if exc:
+            raise exc
+
+    def close(self):
+        """Cleanup resources at exit"""
+        self.logger.debug("Closing exporter")
+        self.flush_queue()
+        self.monitor_batch.clear()
+        self.is_flush_running.clear()
+        self.pool.shutdown()
+        self.logger.debug("Done")
+
+    def run(self):
+        """Entrypoint to flush the queue to BigQuery based on num. records"""
+        while True:
+            if self._buffer.qsize() >= self.CAPACITY and not self.is_flush_running.is_set():
+                self.is_flush_running.set()
+                self.flush_queue()
+                self.is_flush_running.clear()
+            time.sleep(1)
+
+    def flush_queue(self):
+        """Flush the queue to BigQuery"""
+        metrics = []
+
+        while not self._buffer.empty() and len(metrics) <= self.CAPACITY:
+            metrics.append(self._buffer.get())
+
+        if metrics:
+            self.logger.debug(f"Flushing out {len(metrics)} metrics to BigQuery")
+            self.send_metrics(metrics)
+            self._last_flush = time.time()
+
+    @retry(
+        retry=(retry_if_exception_type(queue.Full) | retry_if_exception_type(NoMetricFoundException)),
+        wait=wait_fixed(2) + wait_random(0, 2),
+        stop=stop_after_attempt(5),
+    )
+    def _fetch(self):
+        """Fetch metrics from Airflow"""
+        while True:
+            measure: str = self._sock.recv(1024).decode("utf-8")
+
+            if measure == "":
+                raise NoMetricFoundException("No metrics detected!")
+
+            try:
+                # non blocking
+                self._buffer.put_nowait(PointWithType.from_record(measure))
+            except queue.Full as e:
+                self.logger.error("Queue is full")
+                raise e
+
+    def _get_dfs(self, metrics: t.List[PointWithType]) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
+        """Get dataframes to upload"""
+        measure_dict = self._part_types(metrics)
 
         df_counts, df_last, df_timer = pd.DataFrame(), pd.DataFrame(), pd.DataFrame()
 
@@ -159,36 +247,35 @@ class AirflowMonitor:
         self.logger.info(f"Dimensions: \nCounts: {len(df_counts)} \nLast: {len(df_last)} \nTimers: {len(df_timer)}")
         return df_counts, df_last, df_timer
 
-    def run(self):
+    def send_metrics(self, metrics: t.List[PointWithType]):
         """Entrypoint to run a continuous loop"""
 
-        while True:
-            time.sleep(random.randint(1, 5))
-            df_counts, df_last, df_timer = self._get_dfs()
-            df_counts = self.fix_pd_to_bq_types(df_counts, self.dataset_id, self.counts_table)
-            df_last = self.fix_pd_to_bq_types(df_last, self.dataset_id, self.last_table)
-            df_timer = self.fix_pd_to_bq_types(df_timer, self.dataset_id, self.timers_table)
+        df_counts, df_last, df_timer = self._get_dfs(metrics)
+        df_counts = self.fix_pd_to_bq_types(df_counts, self.dataset_id, self.counts_table)
+        df_last = self.fix_pd_to_bq_types(df_last, self.dataset_id, self.last_table)
+        df_timer = self.fix_pd_to_bq_types(df_timer, self.dataset_id, self.timers_table)
 
-            self.logger.info("Uploading datasets to GBQ")
+        self.logger.debug("Uploading datasets to GBQ")
 
-            if not df_counts.empty:
-                self.gbq_connector.upload_data(df_counts, self.counts_table, self.dataset_id)
+        if not df_counts.empty:
+            self.gbq_connector.upload_data(df_counts, self.counts_table, self.dataset_id)
 
-            if not df_last.empty:
-                self.gbq_connector.upload_data(df_last, self.last_table, self.dataset_id)
+        if not df_last.empty:
+            self.gbq_connector.upload_data(df_last, self.last_table, self.dataset_id)
 
-            if not df_timer.empty:
-                self.gbq_connector.upload_data(df_timer, self.timers_table, self.dataset_id)
+        if not df_timer.empty:
+            self.gbq_connector.upload_data(df_timer, self.timers_table, self.dataset_id)
 
-    def _part_types(self):
+    @staticmethod
+    def _part_types(metrics: t.List[PointWithType]):
         """Update measure type dict with list of records from the buffer"""
 
         measure_dict = defaultdict(list)
-        for record in self._buffer:
+        for record in metrics:
             measure_dict[record.measure].append(record.point)
         return measure_dict
 
-    def fix_pd_to_bq_types(self, df, dataset, table):
+    def fix_pd_to_bq_types(self, df: pd.DataFrame, dataset: str, table: str):
         """Fix pandas to GBQ types"""
 
         schema_fields = self.gbq_connector.retrieve_table_schema(dataset, table, list(df.columns))
